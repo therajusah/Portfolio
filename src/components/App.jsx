bd1f87777870bf41a3c5b9007e493d68c6e840a1
therajusah <workwithrajukumar@gmail.com>
2024-04-21 23:11:51 +0530

    Change mouse follower color to blue

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import  { useEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
 import LocomotiveScroll from "locomotive-scroll";
 import "locomotive-scroll/dist/locomotive-scroll.css";
@@ -92,18 +92,18 @@ function App() {
       </style>
       <MouseFollower />
       <div ref={scrollRef} data-scroll-container>
-        <div className="bg-black min-h-screen">
-          <div className="container mx-auto px-4 md:px-10">
-            <div className="flex justify-between items-center py-8">
-              <h4 className="text-white text-2xl md:text-3xl">Hello</h4>
+        <div className="min-h-screen bg-black">
+          <div className="container px-4 mx-auto md:px-10">
+            <div className="flex items-center justify-between py-8">
+              <h4 className="text-2xl text-white md:text-3xl">Hello</h4>
               <div className="flex gap-6">
-                <h4 className="text-white text-2xl md:text-3xl">Project</h4>
-                <h4 className="text-white text-2xl md:text-3xl">Contact</h4>
+                <h4 className="text-2xl text-white md:text-3xl">Project</h4>
+                <h4 className="text-2xl text-white md:text-3xl">Contact</h4>
               </div>
             </div>
           </div>
-          <div className="container mx-auto px-4 md:px-10">
-            <div className="flex flex-col md:flex-row justify-center items-center">
+          <div className="container px-4 mx-auto md:px-10">
+            <div className="flex flex-col items-center justify-center md:flex-row">
               <div className="md:w-1/2 md:ml-10">
                 <img
                   src={myImage}
@@ -118,19 +118,19 @@ function App() {
                 />
               </div>
               <div
-                className="md:w-1/2 px-4 md:px-10 mt-8 md:mt-0"
+                className="px-4 mt-8 md:w-1/2 md:px-10 md:mt-0"
                 ref={textDivRef}
               >
-                <h3 className="text-white text-3xl md:text-5xl">Hey,</h3>
-                <h3 className="text-white text-3xl md:text-5xl">
+                <h3 className="text-3xl text-white md:text-5xl">Hey,</h3>
+                <h3 className="text-3xl text-white md:text-5xl">
                   I am Raju Kumar
                 </h3>
-                <p className="text-white text-xl md:text-2xl">
+                <p className="text-xl text-white md:text-2xl">
                   A Frontend developer
                 </p>
                 <a
                   href="#contact"
-                  className="text-white text-xl inline-block mt-4 border-b border-white"
+                  className="inline-block mt-4 text-xl text-white border-b border-white"
                 >
                   Get In Touch!
                 </a>
@@ -140,36 +140,36 @@ function App() {
         </div>
 
         <div className="bg-black page-2">
-          <div className="container mx-auto py-24">
+          <div className="container py-24 mx-auto">
             <div className="text-center">
-              <div className="text-white text-4xl font-medium mb-8">
+              <div className="mb-8 text-4xl font-medium text-white">
                 About me
               </div>
-              <p className="text-white text-xl">
+              <p className="text-xl text-white">
                 <>
-                  <h4 className="text-white text-2xl font-medium mb-5">
+                  <h4 className="mb-5 text-2xl font-medium text-white">
                     HTML, CSS, and JavaScript
                   </h4>
 
-                  <h4 className="text-white text-2xl font-medium mb-5">
+                  <h4 className="mb-5 text-2xl font-medium text-white">
                     React, Redux, and GSAP
                   </h4>
 
-                  <h4 className="text-white text-2xl font-medium mb-5">
+                  <h4 className="mb-5 text-2xl font-medium text-white">
                     Tailwind CSS styles,
                   </h4>
 
-                  <h4 className="text-white text-2xl font-medium mb-5">
+                  <h4 className="mb-5 text-2xl font-medium text-white">
                     NodeJS,
                   </h4>
 
-                  <h4 className="text-white text-2xl font-medium mb-5">
+                  <h4 className="mb-5 text-2xl font-medium text-white">
                     MongoDB,
                   </h4>
 
-                  <h4 className="text-white text-2xl font-medium mb-5">Git,</h4>
+                  <h4 className="mb-5 text-2xl font-medium text-white">Git,</h4>
 
-                  <h4 className="text-white text-2xl font-medium mb-5">
+                  <h4 className="mb-5 text-2xl font-medium text-white">
                     Let's craft something remarkable together.
                   </h4>
                 </>
@@ -178,32 +178,32 @@ function App() {
           </div>
         </div>
         <div className="page-3 h-[90vh] w-[100%] bg-black flex flex-col justify-center items-center">
-          <h2 className="text-white text-4xl font-bold mb-8">
+          <h2 className="mb-8 text-4xl font-bold text-white">
             Languages & Frameworks
           </h2>
-          <div className="flex justify-center items-center">
-            <SiHtml5 className="icon html text-6xl mr-8" />
-            <SiCss3 className="icon css text-6xl mr-8" />
-            <SiJavascript className="icon javascript text-6xl mr-8" />
-            <SiReact className="icon react text-6xl mr-8" />
-            <SiTypescript className="icon typescript text-6xl mr-8" />
-            <SiTailwindcss className="icon tailwind text-6xl mr-8" />
-            <SiMongodb className="icon mongodb text-6xl mr-8" />
-            <SiGit className="icon git text-6xl mr-8" />
+          <div className="flex items-center justify-center">
+            <SiHtml5 className="mr-8 text-6xl icon html" />
+            <SiCss3 className="mr-8 text-6xl icon css" />
+            <SiJavascript className="mr-8 text-6xl icon javascript" />
+            <SiReact className="mr-8 text-6xl icon react" />
+            <SiTypescript className="mr-8 text-6xl icon typescript" />
+            <SiTailwindcss className="mr-8 text-6xl icon tailwind" />
+            <SiMongodb className="mr-8 text-6xl icon mongodb" />
+            <SiGit className="mr-8 text-6xl icon git" />
 
           </div>
         </div>
 
         <div className="bg-black">
-          <div className="container mx-auto py-24">
+          <div className="container py-24 mx-auto">
             <div className="flex justify-between">
-              <div className="w-1/4 h-96 bg-blue-500 rounded-lg mx-4"></div>
-              <div className="w-1/4 h-96 bg-blue-500 rounded-lg mx-4"></div>
-              <div className="w-1/4 h-96 bg-blue-500 rounded-lg mx-4"></div>
-              <div className="w-1/4 h-96 bg-blue-500 rounded-lg mx-4"></div>
+              <div className="w-1/4 mx-4 bg-blue-500 rounded-lg h-96"></div>
+              <div className="w-1/4 mx-4 bg-blue-500 rounded-lg h-96"></div>
+              <div className="w-1/4 mx-4 bg-blue-500 rounded-lg h-96"></div>
+              <div className="w-1/4 mx-4 bg-blue-500 rounded-lg h-96"></div>
             </div>
             <div className="flex justify-center mt-16">
-              <div className="text-white text-4xl font-medium">
+              <div className="text-4xl font-medium text-white">
                 Let&apos;s Connect
               </div>
             </div>
@@ -211,31 +211,31 @@ function App() {
               <div className="flex gap-6">
                 <a
                   href="https://www.linkedin.com/in/therajusah"
-                  className="text-white text-4xl"
+                  className="text-4xl text-white"
                 >
                   <RiLinkedinFill />
                 </a>
                 <a
                   href="https://twitter.com/therajusah"
-                  className="text-white text-4xl"
+                  className="text-4xl text-white"
                 >
                   <RiTwitterFill />
                 </a>
                 <a
                   href="https://www.facebook.com/therajusah"
-                  className="text-white text-4xl"
+                  className="text-4xl text-white"
                 >
                   <RiFacebookFill />
                 </a>
                 <a
                   href="https://www.instagram.com/therajusah"
-                  className="text-white text-4xl"
+                  className="text-4xl text-white"
                 >
                   <RiInstagramFill />
                 </a>
                 <a
                   href="https://github.com/therajusah"
-                  className="text-white text-4xl"
+                  className="text-4xl text-white"
                 >
                   <RiGithubFill />
                 </a>
